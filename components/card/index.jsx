15c818bb672c9f2e38f566db7820b4107db9f986
Carlos Gomez <CGomez374@yahoo.com>
2021-06-09 20:16:25 -0700

    add dynamic change of text instead of reusing the code

--- components/card/index.jsx
+++ components/card/index.jsx
@@ -1,16 +1,42 @@
 import styles from './card.module.css'
 import Form from '../form/index'
+import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Index() {
+    //Instead of another page we will set the text to variables
+    const [cardText, setCardText] = useState({
+        signIn: true,
+        h1Title: 'welcome back',
+        h2Title: "sign in",
+        para: 'new to register?',
+        link: 'sign up'
+    });
+
+    // const [signIn, setSignIn] = useState(false);
+
+    const handleClick = () => {
+        setCardText({
+            signIn: false,
+            h1Title: 'register for MPA',
+            h2Title: "sign up",
+            para: 'already have an account?',
+            link: 'sign in'
+        })
+    }
+
     return(
         <div className={styles.cardContainer}>
+            <Head>
+                <title>MPA Sign In</title>
+            </Head>
             <div className={styles.cardLeft}>
                 <div className={styles.contentContainer}>
                    <div className={styles.cardLeftLogo}>
                         <img src="./mp-icon.png" alt="icon" />
                     </div>
                     <div className={styles.innerText}>
-                        <h1>welcome back</h1>
+                        <h1>{cardText.h1Title}</h1>
                         <p>To keep connecting with us please <br /> register with your perfonal info</p>
                     </div> 
                 </div>
@@ -20,8 +46,10 @@ export default function Index() {
                     <div className={styles.imgContainer}>
                         <img src="./closeIcon.png" alt="icon" />
                     </div>
-                    <h2>sign in</h2>
-                    <p>new to register?<a href="#"> sign up</a></p>
+                    <h2>{cardText.h2Title}</h2>
+                    <p>{cardText.para} 
+                        <a href='#' onClick={ handleClick }> {cardText.link}</a>
+                    </p>
                 </div>
                 <ul className={styles.socialMedia}>
                     <li><img src="./google.png" alt="icon" /></li>
@@ -34,7 +62,7 @@ export default function Index() {
                     <p>or</p>
                     <div className={styles.line}></div>
                 </div>
-                <Form />
+                <Form signIn={cardText.signIn}/>
             </div>
         </div>
     )
--- components/form/index.jsx
+++ components/form/index.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import styles from './form.module.css'
 
-export default function Index(){
+export default function Index({ signIn }){
+    /*
+        depending if the signin tag is false or true the 
+        text and input will change; need to figure how to 
+        set the text from the beginning
+    */
+
+
+    const [inputOne, setinputOne] = useState({
+        name: 'email',
+        text: 'email address'
+    });
+
     return(
         <div className={styles.container}>
             <form className={styles.form}>
-                <label htmlFor="email">email address</label><br />
-                <input type="email" name='email'/>
+                <label htmlFor={inputOne.name}> {inputOne.text} </label><br />
+                <input type={inputOne.name} name={inputOne.name}/>
             </form>
             <form className={styles.form}>
                 <label htmlFor="password">password</label><br />
